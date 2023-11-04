b415550cb86a85eccfa9dd07f06fa5b88642e0b0
karryvrcs <114668639+karryvrcs@users.noreply.github.com>
2023-11-04 20:43:26 +0800

    generics

---
+++ src/dev/lpa/ITellYou.java
@@ -0,0 +1,12 @@
+package dev.lpa;
+
+// T is the placeholder for a type that will be specified later.
+// This is called a type identifier, T which is short for Type is commonly used.
+
+record BaseballPlayer(String name, String position){
+
+}
+public class ITellYou <T> {
+    private T field;
+
+}
